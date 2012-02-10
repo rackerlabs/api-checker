951608fe058db3dc815ef8fdcbf7349f564ca7b4
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-10 02:24:56 -0600

    Add an option to check checker against schema.
    This closes #20.

--- src/main/scala/wadl/checker-builder.scala
+++ src/main/scala/wadl/checker-builder.scala
@@ -9,6 +9,7 @@ import java.io.Reader
 import javax.xml.transform._
 import javax.xml.transform.sax._
 import javax.xml.transform.stream._
+import javax.xml.validation._
 
 import org.xml.sax.XMLReader
 import org.xml.sax.InputSource
@@ -32,20 +33,41 @@ class WADLCheckerBuilder(private var wadl : WADLNormalizer) {
 
   def this() = this(null)
 
+  private val schemaFactory = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1")
+
+  //
+  //  Enable CTA full XPath2.0 checking in XSD 1.1
+  //
+  schemaFactory.setFeature ("http://apache.org/xml/features/validation/cta-full-xpath-checking", true)
+
+  val checkerSchema = schemaFactory.newSchema(new StreamSource(getClass().getResourceAsStream("/xsd/checker.xsd")))
+
   val buildTemplates : Templates = wadl.saxTransformerFactory.newTemplates(new StreamSource(getClass().getResourceAsStream("/xsl/builder.xsl")))
   val dupsTemplates : Templates = wadl.saxTransformerFactory.newTemplates(new StreamSource(getClass().getResourceAsStream("/xsl/removeDups.xsl")))
 
-  def build (in : Source, out: Result, removeDups : Boolean) : Unit = {
+  def build (in : Source, out: Result, removeDups : Boolean, validateChecker : Boolean) : Unit = {
     try {
       val transformer = wadl.newTransformer(TREE, XSD11, false, KEEP)
       val buildHandler = wadl.saxTransformerFactory.newTransformerHandler(buildTemplates)
 
+      var output = out;
+
+      if (validateChecker) {
+        val outHandler = wadl.saxTransformerFactory.newTransformerHandler();
+        outHandler.setResult(output)
+
+        val schemaHandler = checkerSchema.newValidatorHandler()
+        schemaHandler.setContentHandler(outHandler)
+
+        output = new SAXResult(schemaHandler)
+      }
+
       if (removeDups) {
         val dupsHandler = wadl.saxTransformerFactory.newTransformerHandler(dupsTemplates)
         buildHandler.setResult (new SAXResult (dupsHandler))
-        dupsHandler.setResult(out)
+        dupsHandler.setResult(output)
       } else {
-        buildHandler.setResult (out)
+        buildHandler.setResult (output)
       }
       transformer.transform (in, new SAXResult(buildHandler))
     } catch {
@@ -53,35 +75,35 @@ class WADLCheckerBuilder(private var wadl : WADLNormalizer) {
     }
   }
 
-  def build(in : (String, InputStream), out: Result, removeDups : Boolean) : Unit = {
+  def build(in : (String, InputStream), out: Result, removeDups : Boolean, validateChecker : Boolean) : Unit = {
     val xmlReader = wadl.newSAXParser.getXMLReader()
     val inputSource = new InputSource(in._2)
     inputSource.setSystemId(in._1)
-    build(new SAXSource(xmlReader, inputSource), out, removeDups)
+    build(new SAXSource(xmlReader, inputSource), out, removeDups, validateChecker)
   }
 
-  def build(in : InputStream, out: Result, removeDups : Boolean) : Unit = {
-    build (("",in), out, removeDups)
+  def build(in : InputStream, out: Result, removeDups : Boolean, validateChecker : Boolean) : Unit = {
+    build (("",in), out, removeDups, validateChecker)
   }
 
-  def build(in : Reader, out: Result, removeDups : Boolean) : Unit = {
+  def build(in : Reader, out: Result, removeDups : Boolean, validateChecker : Boolean) : Unit = {
     val xmlReader = wadl.newSAXParser.getXMLReader()
-    build(new SAXSource(xmlReader, new InputSource(in)), out, removeDups)
+    build(new SAXSource(xmlReader, new InputSource(in)), out, removeDups, validateChecker)
   }
 
-  def build(in : String, out: Result, removeDups : Boolean) : Unit = {
+  def build(in : String, out: Result, removeDups : Boolean, validateChecker : Boolean) : Unit = {
     val xmlReader = wadl.newSAXParser.getXMLReader()
-    build(new SAXSource(xmlReader, new InputSource(in)), out, removeDups)
+    build(new SAXSource(xmlReader, new InputSource(in)), out, removeDups, validateChecker)
   }
 
-  def build (in : (String, NodeSeq), removeDups : Boolean) : NodeSeq = {
+  def build (in : (String, NodeSeq), removeDups : Boolean, validateChecker : Boolean) : NodeSeq = {
     val bytesOut = new ByteArrayOutputStream()
-    build (in, new StreamResult(bytesOut), removeDups)
+    build (in, new StreamResult(bytesOut), removeDups, validateChecker)
     XML.loadString (bytesOut.toString())
   }
 
-  def build (in: NodeSeq, removeDups : Boolean = false) : NodeSeq = {
-    build (("",in), removeDups)
+  def build (in: NodeSeq, removeDups : Boolean = false, validateChecker : Boolean = true) : NodeSeq = {
+    build (("",in), removeDups, validateChecker)
   }
 }
 
