7b60befbd6b12ee6cbdcc7f3e5725d21ecd2e58e
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-12 02:04:52 -0600

    Added a few test to check argument parsing in CLI tools.
    This closes issue #7.

---
+++ cli/wadl2checker/src/test/scala/wadl2checker-tests.scala
@@ -0,0 +1,97 @@
+package com.rackspace.com.papi.components.checker.cli
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+import org.clapper.argot.ArgotUsageException
+
+import javax.xml.transform.stream._
+
+@RunWith(classOf[JUnitRunner])
+class Wadl2CheckerSuite extends FunSuite {
+
+  test ("--help should generate usage info") {
+    Wadl2Checker.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Checker.handleArgs(Array("--help"))
+    }
+  }
+
+  test ("-h should generate usage info") {
+    Wadl2Checker.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Checker.handleArgs(Array("-h"))
+    }
+  }
+
+  test ("-bad_data should generate usage info") {
+    Wadl2Checker.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Checker.handleArgs(Array("-bad_data"))
+    }
+  }
+
+  test ("Too many params should generate usage info") {
+    Wadl2Checker.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Checker.handleArgs(Array("input","output","junk"))
+    }
+  }
+
+  test ("-d should set removeDups") {
+    Wadl2Checker.parser.reset()
+    assert (Wadl2Checker.removeDups.value == None)
+    Wadl2Checker.handleArgs(Array("-d"))
+    assert (Wadl2Checker.removeDups.value.get == true)
+  }
+
+  test ("--remove-dups should set removeDups") {
+    Wadl2Checker.parser.reset()
+    assert (Wadl2Checker.removeDups.value == None)
+    Wadl2Checker.handleArgs(Array("--remove-dups"))
+    assert (Wadl2Checker.removeDups.value.get == true)
+  }
+
+  test ("-v should set validate") {
+    Wadl2Checker.parser.reset()
+    assert (Wadl2Checker.validate.value == None)
+    Wadl2Checker.handleArgs(Array("-v"))
+    assert (Wadl2Checker.validate.value.get == true)
+  }
+
+  test ("--validate should set validate") {
+    Wadl2Checker.parser.reset()
+    assert (Wadl2Checker.validate.value == None)
+    Wadl2Checker.handleArgs(Array("--validate"))
+    assert (Wadl2Checker.validate.value.get == true)
+  }
+
+  test ("no params should set source and result with input/output stream"){
+    Wadl2Checker.parser.reset()
+    Wadl2Checker.handleArgs(Array())
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getInputStream() != null)
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getSystemId() == null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getOutputStream() != null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getSystemId() == null)
+  }
+
+  test ("one params should set source to systemid and result to stream"){
+    Wadl2Checker.parser.reset()
+    Wadl2Checker.handleArgs(Array("test.wadl"))
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getInputStream() == null)
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getSystemId() != null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getOutputStream() != null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getSystemId() == null)
+  }
+
+  test ("two params should set source and result to systemid"){
+    Wadl2Checker.parser.reset()
+    Wadl2Checker.handleArgs(Array("test.wadl", "out.xml"))
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getInputStream() == null)
+    assert (Wadl2Checker.getSource.asInstanceOf[StreamSource].getSystemId() != null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getOutputStream() == null)
+    assert (Wadl2Checker.getResult.asInstanceOf[StreamResult].getSystemId() != null)
+  }
+
+}
---
+++ cli/wadl2dot/src/test/scala/wadl2dot-tests.scala
@@ -0,0 +1,111 @@
+package com.rackspace.com.papi.components.checker.cli
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+import org.clapper.argot.ArgotUsageException
+
+import javax.xml.transform.stream._
+
+@RunWith(classOf[JUnitRunner])
+class Wadl2DotSuite extends FunSuite {
+
+  test ("--help should generate usage info") {
+    Wadl2Dot.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Dot.handleArgs(Array("--help"))
+    }
+  }
+
+  test ("-h should generate usage info") {
+    Wadl2Dot.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Dot.handleArgs(Array("-h"))
+    }
+  }
+
+  test ("-bad_data should generate usage info") {
+    Wadl2Dot.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Dot.handleArgs(Array("-bad_data"))
+    }
+  }
+
+  test ("Too many params should generate usage info") {
+    Wadl2Dot.parser.reset()
+    intercept[ArgotUsageException] {
+      Wadl2Dot.handleArgs(Array("input","output","junk"))
+    }
+  }
+
+  test ("-d should set removeDups") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.removeDups.value == None)
+    Wadl2Dot.handleArgs(Array("-d"))
+    assert (Wadl2Dot.removeDups.value.get == true)
+  }
+
+  test ("--remove-dups should set removeDups") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.removeDups.value == None)
+    Wadl2Dot.handleArgs(Array("--remove-dups"))
+    assert (Wadl2Dot.removeDups.value.get == true)
+  }
+
+  test ("-e should show errors") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.showErrors.value == None)
+    Wadl2Dot.handleArgs(Array("-e"))
+    assert (Wadl2Dot.showErrors.value.get == true)
+  }
+
+  test ("--show-errors should set validate") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.showErrors.value == None)
+    Wadl2Dot.handleArgs(Array("--show-errors"))
+    assert (Wadl2Dot.showErrors.value.get == true)
+  }
+
+  test ("-n should enable nfaMode") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.nfaMode.value == None)
+    Wadl2Dot.handleArgs(Array("-n"))
+    assert (Wadl2Dot.nfaMode.value.get == true)
+  }
+
+  test ("--nfa-mode should enable nfaMode") {
+    Wadl2Dot.parser.reset()
+    assert (Wadl2Dot.nfaMode.value == None)
+    Wadl2Dot.handleArgs(Array("--nfa-mode"))
+    assert (Wadl2Dot.nfaMode.value.get == true)
+  }
+
+  test ("no params should set source and result with input/output stream"){
+    Wadl2Dot.parser.reset()
+    Wadl2Dot.handleArgs(Array())
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getInputStream() != null)
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getSystemId() == null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getOutputStream() != null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getSystemId() == null)
+  }
+
+  test ("one params should set source to systemid and result to stream"){
+    Wadl2Dot.parser.reset()
+    Wadl2Dot.handleArgs(Array("test.wadl"))
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getInputStream() == null)
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getSystemId() != null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getOutputStream() != null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getSystemId() == null)
+  }
+
+  test ("two params should set source and result to systemid"){
+    Wadl2Dot.parser.reset()
+    Wadl2Dot.handleArgs(Array("test.wadl", "out.xml"))
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getInputStream() == null)
+    assert (Wadl2Dot.getSource.asInstanceOf[StreamSource].getSystemId() != null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getOutputStream() == null)
+    assert (Wadl2Dot.getResult.asInstanceOf[StreamResult].getSystemId() != null)
+  }
+
+}
