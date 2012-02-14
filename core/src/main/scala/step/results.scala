e4fde87dbedc0734cd97cd9ce05d46e93d051c68
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-14 13:33:27 -0600

    Initial CheckerResult implementation.

--- core/src/main/scala/step/results.scala
+++ core/src/main/scala/step/results.scala
@@ -5,12 +5,8 @@ package com.rackspace.com.papi.components.checker.step
 //
 abstract class CheckerResult(val message : String, val valid : Boolean, val code : Int)
 
-class AcceptResult extends CheckerResult("Valid", true, 200)
-
-object AcceptResult {
-  val acceptSingleton = new AcceptResult
-  def apply = acceptSingleton
-}
-
+object AcceptResult extends CheckerResult("Valid", true, 200)
 class URLFailResult(message : String) extends CheckerResult(message, false, 404)
 class MethodFailResult(message: String) extends CheckerResult(message, false, 405)
+class ExpectingResult(message: String, code : Int) extends CheckerResult(message, false, code)
+class MultiFailResult(val fails : Array[CheckerResult]) extends CheckerResult ("Multiple possible errors",false,400)
--- core/src/main/scala/step/steps.scala
+++ core/src/main/scala/step/steps.scala
@@ -6,9 +6,8 @@ import javax.servlet.http.HttpServletResponse
 //
 //  Base class for all other steps
 //
-
 abstract class Step(val id : String, val label : String) {
-  def check(req : HttpServletRequest, resp : HttpServletResponse) : Boolean 
+  def check(req : HttpServletRequest, resp : HttpServletResponse) : Option[CheckerResult]
 }
 
 //
@@ -18,11 +17,13 @@ abstract class ConnectedStep(id : String, label : String, val next : Array[Step]
 
   def checkStep(req : HttpServletRequest, resp : HttpServletResponse) : Boolean
 
-  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Boolean = {
+  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Option[CheckerResult] = {
     if (checkStep (req, resp)) {
-      next.foreach {n => if (n.check(req, resp)) return true}
+      val results : Array[Option[CheckerResult]] = next.map (n =>n.check(req, resp)).filter(s => s.isDefined)
+      results.foreach(n => if (n.get.valid) return n)
+      new MultiFailResult (results.map(n=>n.get))
     }
-    false
+    None
   }
 }
 
@@ -37,11 +38,11 @@ class Start(id : String, label : String, next : Array[Step]) extends ConnectedSt
 //  The accept state, send the request over
 //
 class Accept(id : String, label : String) extends Step(id, label) {
-  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Boolean = {
+  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Option[CheckerResult] = {
     //
     //  Send request...
     //
-    return true
+    return Some(AcceptResult)
   }
 }
 
@@ -49,10 +50,23 @@ class Accept(id : String, label : String) extends Step(id, label) {
 //  The URLFail state, return a 404
 //
 class URLFail(id : String, label : String) extends Step(id, label) {
-  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Boolean = {
+  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Option[CheckerResult] = {
     //
-    //  Send 404
+    //  Generate a good error message with context
     //
-    return true
+    return Some(new URLFailResult("Could not find the given resource"))
+  }
+}
+
+//
+// Method fail state
+//
+
+class MethodFail(id : String, label : String) extends Step(id, label) {
+  override def check(req : HttpServletRequest, resp : HttpServletResponse) : Option[CheckerResult] = {
+    //
+    //  Generate a good error message with context
+    //
+    return Some(new MethodFailResult("Expecting method "))
   }
 }
