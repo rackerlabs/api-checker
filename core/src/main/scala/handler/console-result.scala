fe8400563bb3760dd93c967e6881f77793b8512d
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-16 10:34:17 -0600

    Display path in console result handler.

--- core/src/main/scala/handler/console-result.scala
+++ core/src/main/scala/handler/console-result.scala
@@ -5,10 +5,22 @@ import java.io.PrintStream
 import com.rackspace.com.papi.components.checker._
 import com.rackspace.com.papi.components.checker.servlet._
 import com.rackspace.com.papi.components.checker.step.Result
-import com.rackspace.com.papi.components.checker.step.ErrorResult
+import com.rackspace.com.papi.components.checker.step.MultiFailResult
 
-class ConsoleResult(val out : PrintStream=System.out) extends ResultHandler {
+class ConsoleResultHandler(val out : PrintStream=System.out) extends ResultHandler {
   def handle (req : CheckerServletRequest, resp : CheckerServletResponse, result : Result)  : Unit = {
-    out.printf ("%s %s [valid=%s]", (req.getMethod() , req.getRequestURI(), result.valid))
+    out.printf ("%s %s [valid=%s]\n", (req.getMethod() , req.getRequestURI(), result.valid))
+    printPath (req, result)
+    out.println
+  }
+
+  private def printPath (req : CheckerServletRequest, result : Result) : Unit = {
+    out.print ("[")
+    result.stepIDs.foreach (s => out.print(" "+s))
+    if (result.isInstanceOf[MultiFailResult]) {
+      val mfr = result.asInstanceOf[MultiFailResult]
+      mfr.fails.foreach ( f => printPath(req, f))
+    }
+    out.print ("]")
   }
 }
