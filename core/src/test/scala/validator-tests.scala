692ca10d543d3caca416a60fbc4642bb2f1897ef
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-17 15:46:35 -0600

    Add empty machine tests.

--- core/src/test/scala/validator-tests.scala
+++ core/src/test/scala/validator-tests.scala
@@ -7,6 +7,38 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ValidatorSuite extends BaseValidatorSuite {
   //
+  // validator_EMPTY does not allow ANY HTTP requests. The validator
+  // is used in the following tests.
+  //
+  val validator_EMPTY = new Validator({
+    val accept = new Accept("A0", "Accept")
+    val urlFail = new URLFail("UF", "URLFail")
+    val methodFail = new MethodFail ("MF", "MethodFail")
+    val start = new Start("START", "Start", Array(urlFail, methodFail))
+    start
+  }, assertHandler)
+
+  test ("GET on / should fail on validator_EMPTY") {
+    assertResultFailed(validator_EMPTY.validate(request("GET","/"),response))
+  }
+
+  test ("an empty GET should fail on validator_EMPTY") {
+    assertResultFailed(validator_EMPTY.validate(request("GET",""),response))
+  }
+
+  test ("GET on /a should fail on validator_EMPTY") {
+    assertResultFailed(validator_EMPTY.validate(request("GET","/a"),response))
+  }
+
+  test ("POST on /a/b/c/hello/there should fail on validator_EMPTY") {
+    assertResultFailed(validator_EMPTY.validate(request("GET","/a/b/c/hello/there"),response))
+  }
+
+  test ("a completely empty request should fail on validator_EMPTY") {
+    assertResultFailed(validator_EMPTY.validate(request("",""),response))
+  }
+
+  //
   // validator_AB allows a GET on /a/b. The validator is used in the
   // following tests.
   //
@@ -21,39 +53,39 @@ class ValidatorSuite extends BaseValidatorSuite {
     start
   }, assertHandler)
 
-  test ("GET on /a/b should succeed") {
+  test ("GET on /a/b should succeed on validator_AB") {
     validator_AB.validate(request("GET","/a/b"),response)
   }
 
-  test ("GET on /a/b/ should succeed") {
+  test ("GET on /a/b/ should succeed on validator_AB") {
     validator_AB.validate(request("GET","/a/b/"),response)
   }
 
-  test ("GET on a/b/ should succeed") {
+  test ("GET on a/b/ should succeed on validator_AB") {
     validator_AB.validate(request("GET","a/b/"),response)
   }
 
-  test ("GET on / should fail") {
+  test ("GET on / should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("GET","/"),response))
   }
 
-  test ("an empty GET should fail") {
+  test ("an empty GET should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("GET",""),response))
   }
 
-  test ("GET on /a should fail") {
+  test ("GET on /a should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("GET","/a"),response))
   }
 
-  test ("GET on /a/b/c/d should fail") {
+  test ("GET on /a/b/c/d should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("GET","/a/b/c/d"),response))
   }
 
-  test ("POST on /a/b should fail") {
+  test ("POST on /a/b should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("POST","/a/b"),response))
   }
 
-  test ("GET on /index.html should fail") {
+  test ("GET on /index.html should fail on validator_AB") {
     assertResultFailed(validator_AB.validate(request("GET","/index.html"),response))
   }
 
