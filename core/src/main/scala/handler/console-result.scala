720a4f5652815a36aff3c2c7d483c488beb33865
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-17 11:35:36 -0600

    Minor updates to console output.

--- core/src/main/scala/handler/console-result.scala
+++ core/src/main/scala/handler/console-result.scala
@@ -9,18 +9,28 @@ import com.rackspace.com.papi.components.checker.step.MultiFailResult
 
 class ConsoleResultHandler(val out : PrintStream=System.out) extends ResultHandler {
   def handle (req : CheckerServletRequest, resp : CheckerServletResponse, result : Result)  : Unit = {
-    out.printf ("%s %s [valid=%s]\n", (req.getMethod() , req.getRequestURI(), result.valid))
-    printPath (req, result)
-    out.println
+    Console.withOut(out) {
+      def valid(v : Boolean) = {
+        var vout = "["
+        if (v) {vout = vout+Console.GREEN+"VALID"} else {vout = vout+Console.RED+"NOPE"}
+        vout+Console.RESET+"]"
+      }
+
+      printf ("%s %s %s ", valid(result.valid), req.getMethod() , req.getRequestURI())
+      printPath (req, result)
+      println
+    }
   }
 
   private def printPath (req : CheckerServletRequest, result : Result) : Unit = {
-    out.print ("[")
-    result.stepIDs.foreach (s => out.print(" "+s))
-    if (result.isInstanceOf[MultiFailResult]) {
-      val mfr = result.asInstanceOf[MultiFailResult]
-      mfr.fails.foreach ( f => printPath(req, f))
+    Console.withOut(out) {
+      print ("[")
+      result.stepIDs.foreach (s => out.print(" "+s))
+      if (result.isInstanceOf[MultiFailResult]) {
+        val mfr = result.asInstanceOf[MultiFailResult]
+        mfr.fails.foreach ( f => printPath(req, f))
+      }
+      print ("]")
     }
-    out.print ("]")
   }
 }
