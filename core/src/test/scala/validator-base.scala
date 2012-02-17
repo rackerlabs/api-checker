f334158cd58a772d2f6347a4ab68acbb8901ddea
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-17 15:29:51 -0600

    Added assert handler and GET a/b tests.

--- core/src/test/scala/validator-base.scala
+++ core/src/test/scala/validator-base.scala
@@ -5,12 +5,37 @@ import javax.servlet.http.HttpServletResponse
 
 import com.rackspace.com.papi.components.checker.step._
 import com.rackspace.com.papi.components.checker.handler._
+import com.rackspace.com.papi.components.checker.servlet._
 
 import org.scalatest.FunSuite
+import org.scalatest.TestFailedException
 
 import org.mockito.Mockito._
 
+/**
+ * The assert handler throws an ValidationFailedExecption whenever
+ * the request in invalid.
+ */
+class AssertResultHandler extends ResultHandler {
+  def handle (req : CheckerServletRequest, resp : CheckerServletResponse, result : Result)  : Unit = {
+    if (!result.valid) {
+      throw new ResultFailedException("Validation failed",req,resp,result)
+    }
+  }
+}
+
+/**
+ * Exception thrown by the assert result handler,
+ * if a request fails to validate
+ */
+class ResultFailedException(val msg : String, val req : CheckerServletRequest,
+                            val resp : CheckerServletResponse, val result : Result)
+   extends Exception(msg){}
+
 class BaseValidatorSuite extends FunSuite {
+
+  val assertHandler = new DispatchResultHandler(List[ResultHandler](new ConsoleResultHandler(), 
+                                                                    new AssertResultHandler()))
 
   def request(method : String, url : String) : HttpServletRequest = {
     val req = mock(classOf[HttpServletRequest])
@@ -22,4 +47,26 @@ class BaseValidatorSuite extends FunSuite {
 
   def response : HttpServletResponse = mock(classOf[HttpServletResponse]);
 
+  def assertResultFailed(f : => Any) : Unit = {
+    val expectMsg = "Expected validation exception caused by ResultFailed"
+    val result = try {
+      f
+      None
+    } catch {
+      case v : ValidatorException =>
+        val cause = v.getCause()
+        if (cause == null) {
+          throw new TestFailedException(Some(expectMsg+" but got null cause"), None, 4)
+        } else if (!cause.isInstanceOf[ResultFailedException]) {
+          throw new TestFailedException(Some(expectMsg+" but got "+cause), Some(cause), 4)
+        } else {
+          Some(v)
+        }
+      case t : Throwable =>
+        throw new TestFailedException(Some(expectMsg+" but got "+t), Some(t), 4)
+    }
+    if (result == None) {
+      throw new TestFailedException(Some(expectMsg+" but got no exception."), None, 4)
+    }
+  }
 }
--- core/src/test/scala/validator-tests.scala
+++ core/src/test/scala/validator-tests.scala
@@ -1,22 +1,16 @@
 package com.rackspace.com.papi.components.checker
 
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-
 import com.rackspace.com.papi.components.checker.step._
-import com.rackspace.com.papi.components.checker.handler._
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSuite
-
-import org.mockito.Mockito._
-
 
 @RunWith(classOf[JUnitRunner])
 class ValidatorSuite extends BaseValidatorSuite {
-
-  def simpleAB : Step = {
+  //
+  // validator_AB allows a GET on /a/b. The validator is used in the
+  // following tests.
+  //
+  val validator_AB = new Validator({
     val accept = new Accept("A0", "Accept")
     val urlFail = new URLFail("UF", "URLFail")
     val methodFail = new MethodFail ("MF", "MethodFail")
@@ -25,14 +19,42 @@ class ValidatorSuite extends BaseValidatorSuite {
     val a = new URI("a","a", "a".r, Array(b, urlFail, methodFail))
     val start = new Start("START", "Start", Array(a, urlFail, methodFail))
     start
+  }, assertHandler)
+
+  test ("GET on /a/b should succeed") {
+    validator_AB.validate(request("GET","/a/b"),response)
   }
 
-  test ("validate a simple GET on a/b") {
-    val validator = new Validator(simpleAB, new ConsoleResultHandler())
-    validator.validate(request("GET","/a/b"),response)
-    validator.validate(request("GET","/a"),response)
-    validator.validate(request("GET","/a/b/c/d"),response)
-    validator.validate(request("POST", "/a/b"),response)
-    validator.validate(request("GET", "/index.html"), response)
+  test ("GET on /a/b/ should succeed") {
+    validator_AB.validate(request("GET","/a/b/"),response)
   }
+
+  test ("GET on a/b/ should succeed") {
+    validator_AB.validate(request("GET","a/b/"),response)
+  }
+
+  test ("GET on / should fail") {
+    assertResultFailed(validator_AB.validate(request("GET","/"),response))
+  }
+
+  test ("an empty GET should fail") {
+    assertResultFailed(validator_AB.validate(request("GET",""),response))
+  }
+
+  test ("GET on /a should fail") {
+    assertResultFailed(validator_AB.validate(request("GET","/a"),response))
+  }
+
+  test ("GET on /a/b/c/d should fail") {
+    assertResultFailed(validator_AB.validate(request("GET","/a/b/c/d"),response))
+  }
+
+  test ("POST on /a/b should fail") {
+    assertResultFailed(validator_AB.validate(request("POST","/a/b"),response))
+  }
+
+  test ("GET on /index.html should fail") {
+    assertResultFailed(validator_AB.validate(request("GET","/index.html"),response))
+  }
+
 }
