965161c4c1863b1b350936fc93b5d9f9efa888b1
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-18 22:28:30 -0600

    Added accept test.

--- core/src/test/scala/step/step-tests.scala
+++ core/src/test/scala/step/step-tests.scala
@@ -6,6 +6,19 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class StepSuite extends BaseStepSuite {
 
+  test("Regardless of input, Accept step should always return AcceptResult") {
+    val accept = new Accept("a","a")
+    val res2 = accept.check(request("GET", "/a/b"), response, 2)
+    assert(res2.isDefined)
+    assert(res2.get.isInstanceOf[AcceptResult])
+    val res3 = accept.check(request("XGET", "/a/b"), response, 0)
+    assert(res3.isDefined)
+    assert(res3.get.isInstanceOf[AcceptResult])
+    val res = accept.check (null, null, -1)
+    assert(res.isDefined)
+    assert(res.get.isInstanceOf[AcceptResult])
+  }
+
   test("Method should return method fail result if the URI level has been exceeded") {
     val mf  = new MethodFail("mf", "mf")
     val res = mf.check (request("GET", "/a/b"), response, 2)
