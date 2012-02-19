2ce311bfaad259ea50e73a6b1f3df521432bac77
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-18 22:18:56 -0600

    Added URLFail step tests.

--- core/src/test/scala/step/step-tests.scala
+++ core/src/test/scala/step/step-tests.scala
@@ -24,4 +24,22 @@ class StepSuite extends BaseStepSuite {
     assert (res2 == None)
   }
 
+  test("URLFail should return URL fail result if URI level has not been exceeded") {
+    val uf = new URLFail("uf", "uf")
+    val res = uf.check (request("GET", "/a/b"), response, 0)
+    assert (res.isDefined)
+    assert (res.get.isInstanceOf[URLFailResult])
+    val res2 = uf.check (request("GET", "/a/b"), response, 1)
+    assert (res2.isDefined)
+    assert (res2.get.isInstanceOf[URLFailResult])
+  }
+
+  test("URLFail should return None if URI level has been exceeded") {
+    val uf = new URLFail("uf", "uf")
+    val res = uf.check (request("GET", "/a/b"), response, 2)
+    assert (res == None)
+    val res2 = uf.check (request("GET", "/a/b"), response, 3)
+    assert (res2 == None)
+  }
+
 }
