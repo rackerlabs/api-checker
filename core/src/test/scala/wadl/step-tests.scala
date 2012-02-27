840d8d0a10283deb6937f0838fcbc73940d300ef
Jorge L. Williams <jorge.williams@rackspace.com>
2012-02-27 01:07:06 -0600

    Picked test scenarios. Still need to fill in assertions.
    Issue #9.

--- core/src/test/scala/wadl/step-tests.scala
+++ core/src/test/scala/wadl/step-tests.scala
@@ -31,5 +31,193 @@ class WADLStepSpec extends BaseStepSpec {
       assert (step.next.filter(a => a.isInstanceOf[URLFail]).length == 1)
       assert (step.next.filter(a => a.isInstanceOf[MethodFail]).length == 1)
     }
+
+    scenario("The WADL contains a single multi-path resource") {
+      given("a WADL that contains a single multi-path resource with a GET and DELETE method")
+      val inWADL =
+        <application xmlns="http://wadl.dev.java.net/2009/02">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/resource">
+                   <method name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method name="DELETE">
+                      <response status="200"/>
+                   </method>
+              </resource>
+           </resources>
+        </application>
+      when("the wadl is translated")
+      val step = builder.build (inWADL)
+      //
+      // add assertions
+      //
+    }
+
+    scenario("The WADL contains multiple, related paths") {
+      given ("a WADL with multiple related paths")
+      val inWADL =
+        <application xmlns="http://wadl.dev.java.net/2009/02">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/resource">
+                   <method name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method name="DELETE">
+                      <response status="200"/>
+                   </method>
+              </resource>
+              <resource path="path/to/my/other_resource">
+                   <method name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method name="POST">
+                      <response status="200"/>
+                   </method>
+              </resource>
+          </resources>
+        </application>
+      when("the wadl is translated")
+      val step = builder.build (inWADL)
+      //
+      // add assertions
+      //
+    }
+
+    scenario("The WADL contains multiple, unrelated paths") {
+      given ("a WADL with multiple unrelated paths")
+      val inWADL =
+        <application xmlns="http://wadl.dev.java.net/2009/02">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/resource">
+                   <method name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method name="DELETE">
+                      <response status="200"/>
+                   </method>
+              </resource>
+              <resource path="this/is/my/other_resource">
+                   <method name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method name="POST">
+                      <response status="200"/>
+                   </method>
+              </resource>
+          </resources>
+        </application>
+      when("the wadl is translated")
+      val step = builder.build (inWADL)
+      //
+      //  Add assertions
+      //
+    }
+
+    scenario("The WADL contains method ids") {
+      given ("a WADL with method IDs")
+      val inWADL =
+        <application xmlns="http://wadl.dev.java.net/2009/02">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/resource">
+                   <method id="getResource" name="GET">
+                      <response status="200 203"/>
+                   </method>
+                   <method id="deleteResource" name="DELETE">
+                      <response status="200"/>
+                   </method>
+              </resource>
+          </resources>
+        </application>
+      when("the wadl is translated")
+      val step = builder.build (inWADL)
+      //
+      //  Add assertions
+      //
+    }
+
+    scenario("The WADL contains an initial invisible node") {
+      given ("a WADL with an initial invisble node")
+      val inWADL =
+        <application xmlns="http://wadl.dev.java.net/2009/02"
+                     xmlns:rax="http://docs.rackspace.com/api">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource rax:invisible="true" path="path">
+               <method name="GET">
+                    <response status="200 203"/>
+                </method>
+                <resource path="to">
+                  <resource path="my">
+                   <resource path="resource">
+                     <method name="GET">
+                        <response status="200 203"/>
+                     </method>
+                     <method name="DELETE">
+                        <response status="200"/>
+                     </method>
+                   </resource>
+                 </resource>
+                </resource>
+              </resource>
+           </resources>
+        </application>
+      when ("the wadl is translated")
+      val step = builder.build (inWADL)
+      //
+      // Add assertions
+      //
+    }
+
+    scenario("The WADL contains a template parameter of type string at the end of a path") {
+      given("a WADL with a single template string at the end of the path")
+      val inWADL=
+        <application xmlns="http://wadl.dev.java.net/2009/02"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/resource/{id}">
+                   <param name="id" style="template" type="xsd:string"/>
+                   <method href="#getMethod" />
+              </resource>
+           </resources>
+           <method id="getMethod" name="GET">
+               <response status="200 203"/>
+           </method>
+        </application>
+      when ("the wadl is translated")
+      val checker = builder.build (inWADL)
+      //
+      //  Add assertions
+      //
+    }
+
+    scenario("The WADL contains a template parameter of type string in the middle of the path") {
+      given("a WADL with a single template string in the middle of the path")
+      val inWADL=
+        <application xmlns="http://wadl.dev.java.net/2009/02"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+           <grammars/>
+           <resources base="https://test.api.openstack.com">
+              <resource path="path/to/my/{id}/resource">
+                   <param name="id" style="template" type="xsd:string"/>
+                   <method href="#getMethod" />
+              </resource>
+           </resources>
+           <method id="getMethod" name="GET">
+               <response status="200 203"/>
+           </method>
+        </application>
+      when ("the wadl is translated")
+      val checker = builder.build (inWADL)
+      //
+      //  Add assertions
+      //
+    }
+
   }
 }
