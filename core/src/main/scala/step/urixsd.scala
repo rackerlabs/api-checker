e4e26a097e5aa382ce2853cbd058b8b189265f1f
Jorge L. Williams <jorge.williams@rackspace.com>
2012-03-02 02:27:12 -0600

    inital urixsd state.

---
+++ core/src/main/scala/step/urixsd.scala
@@ -0,0 +1,23 @@
+package com.rackspace.com.papi.components.checker.step
+
+import javax.xml.namespace.QName
+import javax.xml.validation.Schema
+
+import com.rackspace.com.papi.components.checker.servlet._
+
+class URIXSD(id : String, label : String, sType : QName, sch : Schema, next : Array[Step])  extends ConnectedStep(id, label, next)
+   with XSDStringValidator {
+
+     override val mismatchMessage : String = simpleType.toString;
+     override val elementName : String = id
+     override val simpleType : QName = sType
+     override val schema : Schema = sch
+
+     override def checkStep(req : CheckerServletRequest, resp : CheckerServletResponse, uriLevel : Int) : Int = {
+       var ret = -1
+       if ((uriLevel < req.URISegment.size) && validate(req.URISegment(uriLevel))) {
+         ret = uriLevel+1
+       }
+       ret
+     }
+}
---
+++ core/src/main/scala/step/xsd-string-validator.scala
@@ -0,0 +1,59 @@
+package com.rackspace.com.papi.components.checker.step
+
+import javax.xml.namespace.QName
+import javax.xml.validation.Schema
+import javax.xml.validation.ValidatorHandler
+
+import org.xml.sax.SAXParseException
+import org.xml.sax.ErrorHandler
+import org.xml.sax.Attributes
+
+import org.xml.sax.helpers.AttributesImpl
+
+trait XSDStringValidator {
+  val simpleType : QName
+  val schema : Schema
+  val elementName : String
+  val attributes : Attributes = {
+    val ah = new AttributesImpl()
+    ah.addAttribute ("http://www.w3.org/2001/XMLSchema-instance", "type",
+                     "xsi:type", "", simpleType.toString)
+    ah
+  }
+
+  def validate (in : String) : Boolean = {
+    val capture = new ErrorCapture
+    val handler = schema.newValidatorHandler
+    val inArray = in.toCharArray()
+
+    handler.setErrorHandler(capture)
+    handler.startDocument
+    handler.startPrefixMapping(simpleType.getPrefix, simpleType.getNamespaceURI)
+    handler.startPrefixMapping("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+    handler.startElement("", elementName, elementName, attributes)
+    handler.characters(inArray, 0, inArray.length)
+    handler.endElement("", elementName, elementName)
+    handler.endPrefixMapping(simpleType.getPrefix)
+    handler.endPrefixMapping("xsi")
+    handler.endDocument
+
+    capture.error != None
+  }
+}
+
+//
+//  An error handler that simply captures the last error.
+//
+private class ErrorCapture extends ErrorHandler {
+  var error : Option[SAXParseException] = None
+
+  def error(exception : SAXParseException) : Unit = {
+    error = Some(exception)
+  }
+
+  def fatalError(exception : SAXParseException) : Unit = {
+    error = Some(exception)
+  }
+
+  def warning(exception : SAXParseException) : Unit = {} //Log?
+}
