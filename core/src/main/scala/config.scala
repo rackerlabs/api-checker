c33e924fcdf95cd3d52bbf9b7634c7903867ffa3
Jorge L. Williams <jorge.williams@rackspace.com>
2012-05-26 18:00:44 -0500

    Validator now has a config object.  This closes #34.

---
+++ core/src/main/scala/config.scala
@@ -0,0 +1,18 @@
+package com.rackspace.com.papi.components.checker
+
+import scala.reflect.BeanProperty
+
+import com.rackspace.com.papi.components.checker.handler.ResultHandler
+import com.rackspace.com.papi.components.checker.handler.ServletResultHandler
+
+class Config {
+  //
+  //  Don't allow duplicate nodes in the machine.
+  //
+  @BeanProperty var removeDups : Boolean = true
+
+  //
+  //  The result handler, recives and manages all results.
+  //
+  @BeanProperty var resultHandler : ResultHandler = new ServletResultHandler
+}
