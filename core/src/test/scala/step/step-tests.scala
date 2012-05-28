fa6c709f0c0894d6de37dda60d730a5c11fafac6
Jorge L. Williams <jorge.williams@rackspace.com>
2012-05-28 01:06:11 -0500

    Initial implementation of ReqType step.

--- core/src/test/scala/step/step-tests.scala
+++ core/src/test/scala/step/step-tests.scala
@@ -367,4 +367,26 @@ class StepSuite extends BaseStepSuite {
     assert (rtf.check (request("POST", "/index.html", "application/xml"), response, chain, 0) == None)
   }
 
+  test("ReqType mismatch message should be the same of the type regex") {
+    val rt = new ReqType ("XML", "XML", "application/xml|application/json".r, Array[Step]())
+    assert (rt.mismatchMessage == "application/xml|application/json".r.toString)
+  }
+
+  test("In a ReqType step, if the content type does not match, the returned URI level should be -1") {
+    val rt = new ReqType ("XML", "XML", "application/xml|application/json".r, Array[Step]())
+    assert (rt.checkStep (request("PUT", "/a/b","*.*"), response,chain, 0) == -1)
+    assert (rt.checkStep (request("POST", "/a/b","application/junk"), response,chain, 1) == -1)
+    val rt2 = new ReqType ("XML", "XML", "text/html".r, Array[Step]())
+    assert (rt2.checkStep (request("PUT", "/a/b","*.*"), response,chain, 0) == -1)
+    assert (rt2.checkStep (request("POST", "/a/b","application/junk"), response,chain, 2) == -1)
+  }
+
+  test("In a ReqType step, if the content matches, the URI level should stay the same") {
+    val rt = new ReqType ("XML", "XML", "application/xml|application/json".r, Array[Step]())
+    assert (rt.checkStep (request("PUT", "/a/b","application/xml"), response,chain, 0) == 0)
+    assert (rt.checkStep (request("POST", "/a/b","application/json"), response,chain, 1) == 1)
+    val rt2 = new ReqType ("XML", "XML", "text/html".r, Array[Step]())
+    assert (rt2.checkStep (request("GET", "/a/b/c","text/html"), response,chain, 2) == 2)
+  }
+
 }
