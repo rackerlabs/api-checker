75e553772ec09f129a5c25ae0d7748f09c11d3e3
Jorge L. Williams <jorge.williams@rackspace.com>
2012-06-08 00:50:10 -0500

    Inital parser pool.

---
+++ core/src/main/scala/util/parser-pool.scala
@@ -0,0 +1,54 @@
+package com.rackspace.com.papi.components.checker.util
+
+import javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.DocumentBuilder
+
+import org.apache.commons.pool.PoolableObjectFactory
+import org.apache.commons.pool.impl.SoftReferenceObjectPool
+
+object XMLParserPool {
+  private val pool = new SoftReferenceObjectPool[DocumentBuilder](new XMLParserFactory)
+
+  def borrowParser : DocumentBuilder = pool.borrowObject()
+  def returnParser (builder : DocumentBuilder) : Unit = pool.returnObject(builder)
+  def numActive : Int = pool.getNumActive()
+  def numIdle : Int = pool.getNumIdle()
+}
+
+private class XMLParserFactory extends PoolableObjectFactory[DocumentBuilder] {
+  val builderFactory = DocumentBuilderFactory.newInstance ("org.apache.xerces.jaxp.DocumentBuilderFactoryImpl", null)
+
+  //
+  //  Setup the builder factory so that it works within the security
+  //  constraints of a webservice.
+  //
+  builderFactory.setCoalescing(true)
+  builderFactory.setIgnoringComments(true)
+  builderFactory.setNamespaceAware(true)
+  builderFactory.setValidating(false)
+  builderFactory.setXIncludeAware(false)
+  builderFactory.setExpandEntityReferences (false)
+  builderFactory.setFeature (FEATURE_SECURE_PROCESSING, true)
+
+  def makeObject = builderFactory.newDocumentBuilder()
+
+  def activateObject (builder : DocumentBuilder) : Unit = {
+    //
+    //  No need to activate the parser should be ready to go.
+    //
+  }
+
+  def validateObject (builder : DocumentBuilder) : Boolean = builder != null
+
+  def passivateObject (builder : DocumentBuilder) : Unit = {
+    builder.reset()
+  }
+
+  def destroyObject (builder : DocumentBuilder) : Unit = {
+    //
+    //  Not needed...
+    //
+  }
+
+}
