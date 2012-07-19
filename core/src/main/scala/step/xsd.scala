64f435c24adcdc62d770d0952b4ab414805115c2
Jorge L. Williams <jorge.williams@rackspace.com>
2012-07-19 15:47:06 -0500

    Use our own prebuilt parser rather than letting saxon build one.

--- core/src/main/scala/step/xsd.scala
+++ core/src/main/scala/step/xsd.scala
@@ -6,12 +6,18 @@ import javax.xml.validation.Validator
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.dom.DOMResult
 
+import javax.xml.parsers.DocumentBuilder
+
 import javax.servlet.FilterChain
 
 import org.w3c.dom.Document
 
 import com.rackspace.com.papi.components.checker.servlet._
-import com.rackspace.com.papi.components.checker.util.ValidatorPool._
+
+import com.rackspace.com.papi.components.checker.util.ValidatorPool.borrowValidator
+import com.rackspace.com.papi.components.checker.util.ValidatorPool.returnValidator
+import com.rackspace.com.papi.components.checker.util.XMLParserPool.borrowParser
+import com.rackspace.com.papi.components.checker.util.XMLParserPool.returnParser
 
 class XSD(id : String, label : String, schema : Schema, transform : Boolean, next : Array[Step]) extends ConnectedStep(id, label, next) {
   override val mismatchMessage : String = "The XML does not validate against the schema."
@@ -19,17 +25,26 @@ class XSD(id : String, label : String, schema : Schema, transform : Boolean, nex
   override def checkStep(req : CheckerServletRequest, resp : CheckerServletResponse, chain : FilterChain, uriLevel : Int) : Int = {
     var ret = -1
     var validator : Validator = null
+    var parser : DocumentBuilder = null
     val capture = new ErrorCapture //Used to capture parse errors
     var error : Exception = null   //Other errors may be caught here
 
     try {
-      validator = borrowValidator (schema)
+      validator = borrowValidator(schema)
       validator.setErrorHandler(capture)
-      if (transform) {
-        val result = new DOMResult
 
-        validator.validate (new DOMSource (req.parsedXML), result)
-        req.parsedXML = result.getNode().asInstanceOf[Document]
+      if (transform) {
+        //
+        //  We create a new document because Saxon doesn't pool
+        //  DocumentBuilders and letting saxon create a new builder
+        //  slows things down.
+        //
+        parser = borrowParser
+        val result = parser.newDocument()
+        returnParser(parser); parser = null
+
+        validator.validate (new DOMSource (req.parsedXML), new DOMResult(result))
+        req.parsedXML = result
       } else {
         validator.validate (new DOMSource (req.parsedXML))
       }
@@ -38,6 +53,7 @@ class XSD(id : String, label : String, schema : Schema, transform : Boolean, nex
       case e : Exception => error = e
     } finally {
       if (validator != null) returnValidator (schema, validator)
+      if (parser != null) returnParser(parser)
     }
 
     //
