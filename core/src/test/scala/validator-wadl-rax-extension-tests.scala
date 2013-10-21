bfddc05fc104c5aec2805156bda9be00da92a28d
Lisa Clark <lisa.clark@rackspace.com>
2013-10-21 16:05:23 -0500

    raxroles: test support for nested resource

--- core/src/test/scala/validator-wadl-rax-extension-tests.scala
+++ core/src/test/scala/validator-wadl-rax-extension-tests.scala
@@ -138,17 +138,49 @@ class GivenAWadlWithRolesAtResourceLevel extends FlatSpec with RaxRolesBehaviors
                  xmlns:rax="http://docs.rackspace.com/api">
       <resources base="https://test.api.openstack.com">
         <resource path="/a" rax:roles="foo:creator">
-          <method name="POST">
-            <request>
-              <representation mediaType="application/xml"/>
-            </request>
-          </method>
-          <method name="GET">
-            <request>
-              <representation mediaType="application/xml"/>
-            </request>
-          </method>
-        </resource>
+        <method name="POST">
+          <request>
+            <representation mediaType="application/xml"/>
+          </request>
+        </method>
+        <method name="GET">
+          <request>
+            <representation mediaType="application/xml"/>
+          </request>
+        </method>
+        <method name="PUT" rax:roles="foo:observer">
+          <request>
+            <representation mediaType="application/xml"/>
+          </request>
+        </method>
+        <method name="DELETE" rax:roles="foo:observer foo:creator foo:admin">
+          <request>
+            <representation mediaType="application/xml"/>
+          </request>
+        </method>
+          <resource path="/b" rax:roles="b:creator">
+            <method name="POST">
+              <request>
+                <representation mediaType="application/xml"/>
+              </request>
+            </method>
+            <method name="GET">
+              <request>
+                <representation mediaType="application/xml"/>
+              </request>
+            </method>
+            <method name="PUT" rax:roles="b:observer">
+              <request>
+                <representation mediaType="application/xml"/>
+              </request>
+            </method>
+            <method name="DELETE" rax:roles="b:observer b:creator b:admin">
+              <request>
+                <representation mediaType="application/xml"/>
+              </request>
+            </method>
+          </resource>
+      </resource>
       </resources>
     </application>)
     , TestConfig(false, false, true, true, true, 1, true, true, true, "XalanC", true, true))
@@ -156,9 +188,70 @@ class GivenAWadlWithRolesAtResourceLevel extends FlatSpec with RaxRolesBehaviors
   // When a single value rax:roles at resource level but not at method level
   it should behave like allowAccess(validator, "GET", "/a", List("foo:creator"))
   it should behave like preventAccess(validator, "GET", "/a", List("foo:observer"))
+  it should behave like preventAccess(validator, "GET", "/a", List("b:observer"))
+  it should behave like preventAccess(validator, "GET", "/a", List("b:creator"))
   it should behave like preventAccessWhenNoXRoles(validator, "GET", "/a")
 
   it should behave like allowAccess(validator, "POST", "/a", List("foo:creator"))
   it should behave like preventAccess(validator, "POST", "/a", List("foo:observer"))
   it should behave like preventAccessWhenNoXRoles(validator, "POST", "/a")
+
+  // PUT has resource level foo:creator, method level foo:observer
+  it should behave like allowAccess(validator, "PUT", "/a", List("foo:creator"))
+  it should behave like allowAccess(validator, "PUT", "/a", List("foo:observer"))
+  it should behave like allowAccess(validator, "PUT", "/a", List("foo:observer", "foo:creator"))
+
+  it should behave like preventAccess(validator, "PUT", "/a", List("foo:bar"))
+  it should behave like preventAccess(validator, "PUT", "/a", List())
+  it should behave like preventAccess(validator, "PUT", "/a", List("foo:observe"))
+  it should behave like preventAccessWhenNoXRoles(validator, "PUT", "/a")
+
+  // DELETE has resource level foo:creator, method level foo:observer and foo:creator
+  it should behave like allowAccess(validator, "DELETE", "/a", List("foo:creator"))
+  it should behave like allowAccess(validator, "DELETE", "/a", List("foo:observer"))
+  it should behave like allowAccess(validator, "DELETE", "/a", List("foo:observer", "foo:creator"))
+  it should behave like allowAccess(validator, "DELETE", "/a", List("foo:admin"))
+
+  it should behave like preventAccess(validator, "DELETE", "/a", List("foo:bar"))
+  it should behave like preventAccess(validator, "DELETE", "/a", List())
+  it should behave like preventAccess(validator, "DELETE", "/a", List("foo:observe"))
+  it should behave like preventAccessWhenNoXRoles(validator, "DELETE", "/a")
+
+  // Sub-resource b: inherits resource a foo:creator access
+  // When a single value rax:roles at resource level but not at method level
+  it should behave like allowAccess(validator, "GET", "/a/b", List("foo:creator"))
+  it should behave like allowAccess(validator, "GET", "/a/b", List("b:creator"))
+  it should behave like preventAccess(validator, "GET", "/a/b", List("foo:observer"))
+  it should behave like preventAccessWhenNoXRoles(validator, "GET", "/a/b")
+
+  it should behave like allowAccess(validator, "POST", "/a/b", List("foo:creator"))
+  it should behave like allowAccess(validator, "POST", "/a/b", List("b:creator"))
+  it should behave like preventAccess(validator, "POST", "/a/b", List("foo:observer"))
+  it should behave like preventAccessWhenNoXRoles(validator, "POST", "/a/b")
+
+  // PUT has resource level b:creator, method level b:observer, parent resource foo:creator
+  it should behave like allowAccess(validator, "PUT", "/a/b", List("foo:creator"))
+  it should behave like allowAccess(validator, "PUT", "/a/b", List("b:observer"))
+  it should behave like allowAccess(validator, "PUT", "/a/b", List("b:observer", "foo:creator"))
+
+  it should behave like preventAccess(validator, "PUT", "/a/b", List("foo:observer"))
+  it should behave like preventAccess(validator, "PUT", "/a/b", List("foo:bar"))
+  it should behave like preventAccess(validator, "PUT", "/a/b", List())
+  it should behave like preventAccess(validator, "PUT", "/a/b", List("b:observe"))
+  it should behave like preventAccess(validator, "PUT", "/a/b", List("observer"))
+  it should behave like preventAccessWhenNoXRoles(validator, "PUT", "/a/b")
+
+  // DELETE has parent resource level foo:creator, method level b:observer and b:creator and b:admin
+  it should behave like allowAccess(validator, "DELETE", "/a/b", List("foo:creator"))
+  it should behave like allowAccess(validator, "DELETE", "/a/b", List("b:observer"))
+  it should behave like allowAccess(validator, "DELETE", "/a/b", List("b:observer", "foo:creator"))
+  it should behave like allowAccess(validator, "DELETE", "/a/b", List("b:admin"))
+
+  it should behave like preventAccess(validator, "DELETE", "/a/b", List("foo:bar"))
+  it should behave like preventAccess(validator, "DELETE", "/a/b", List())
+  it should behave like preventAccess(validator, "DELETE", "/a/b", List("foo:observe"))
+  it should behave like preventAccess(validator, "DELETE", "/a/b", List("foo:observer"))
+  it should behave like preventAccess(validator, "DELETE", "/a/b", List("foo:admin"))
+
+  it should behave like preventAccessWhenNoXRoles(validator, "DELETE", "/a/b")
 }
