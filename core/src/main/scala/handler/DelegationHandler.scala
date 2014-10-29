cb9f2cae2a273497d910af3dc33058f9d6c977f6
Damien Johnson <damien.johnson@rackspace.com>
2014-10-29 16:46:31 -0500

    Implemented delegation handler utilizing the delegation library

---
+++ core/src/main/scala/handler/DelegationHandler.scala
@@ -0,0 +1,48 @@
+  /***
+ *   Copyright 2014 Rackspace US, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.rackspace.com.papi.components.checker.handler
+
+import javax.servlet.FilterChain
+
+import com.rackspace.com.papi.components.checker.Validator
+import com.rackspace.com.papi.components.checker.servlet.{CheckerServletRequest, CheckerServletResponse}
+import com.rackspace.com.papi.components.checker.step.{ErrorResult, Result}
+import com.rackspace.httpdelegation.impl.HttpDelegationManagerImpl.buildDelegationHeaders
+import org.w3c.dom.Document
+
+class DelegationHandler(delegationQuality: Double) extends ResultHandler {
+
+  def init (validator : Validator, checker : Option[Document]) : Unit = {}
+
+  def handle (req : CheckerServletRequest, resp : CheckerServletResponse, chain : FilterChain, result : Result) : Unit = {
+    if (!result.valid) {
+      result match {
+        case errorResult : ErrorResult =>
+          val delegationHeaders = buildDelegationHeaders(
+            errorResult.code, "api-checker", errorResult.message, delegationQuality
+          )
+
+          delegationHeaders.foreach { case (headerName, headerValues) =>
+            headerValues.foreach { headerValue =>
+              req.addHeader(headerName, headerValue)
+            }
+          }
+
+          chain.doFilter(req, resp)
+      }
+    }
+  }
+}
