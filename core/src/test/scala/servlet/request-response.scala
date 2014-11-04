9574a3b870ca64ba27cfc7f0e4fd7e3ba028aa57
Damien Johnson <damien.johnson@rackspace.com>
2014-11-04 14:20:29 -0600

    Updated CheckerServletRequest to allow header mutation

--- core/src/test/scala/servlet/request-response.scala
+++ core/src/test/scala/servlet/request-response.scala
@@ -16,6 +16,9 @@
 package com.rackspace.com.papi.components.checker
 
 import com.rackspace.com.papi.components.checker.servlet.CheckerServletRequest
+import org.mockito.Mockito.when
+
+import scala.collection.JavaConverters._
 
 class RequestResponseSuite extends BaseValidatorSuite {
 
@@ -31,4 +34,71 @@ class RequestResponseSuite extends BaseValidatorSuite {
     assert(wrap.getHeaders("User-Agent").nextElement()=="Bla, bla bla")
   }
 
+  test("Ensure headers added via addHeader are accessible via getHeader") {
+    val req = request("GET","/foo","application/XML","",false,Map("a"->List("1")))
+    val wrap = new CheckerServletRequest(req)
+    wrap.addHeader("b", "2")
+    assert(wrap.getHeader("b")=="2")
+  }
+
+  test("Ensure headers added via addHeader are accessible via getHeader in a case-insensitive manner") {
+    val req = request("GET","/foo","application/XML","",false,Map("a"->List("1")))
+    val wrap = new CheckerServletRequest(req)
+    wrap.addHeader("b", "2")
+    assert(wrap.getHeader("B")=="2")
+  }
+
+  test("Ensure headers added via addHeader are accessible via getHeaders") {
+    val req = request("GET","/foo","application/XML","",false,Map("a"->List("1")))
+    val wrap = new CheckerServletRequest(req)
+    wrap.addHeader("a", "2")
+    wrap.addHeader("b", "2")
+    wrap.addHeader("b", "3")
+
+    val headerValuesA = wrap.getHeaders("a").asScala.toSet
+    val headerValuesB = wrap.getHeaders("b").asScala.toSet
+    assert(headerValuesA.contains("1"))
+    assert(headerValuesA.contains("2"))
+    assert(headerValuesB.contains("2"))
+    assert(headerValuesB.contains("3"))
+  }
+
+  test("Ensure getHeaderNames returns all header names") {
+    val req = request("GET","/foo","application/XML","",false,Map("a"->List("1")))
+    when(req.getHeaderNames).thenReturn(Iterator("a").asJavaEnumeration)
+
+    val wrap = new CheckerServletRequest(req)
+    wrap.addHeader("b", "2")
+
+    val headerNameSet = wrap.getHeaderNames.asScala.toSet
+    assert(headerNameSet.contains("a"))
+    assert(headerNameSet.contains("b"))
+  }
+
+  test("Ensure getIntHeader returns the Int value of a header value") {
+    val req = request("GET","/foo","application/XML","",false,Map("a"->List("1")))
+    when(req.getIntHeader("a")).thenReturn(1)
+
+    val wrap = new CheckerServletRequest(req)
+    wrap.addHeader("b", "2")
+
+    assert(wrap.getIntHeader("a")==1)
+    assert(wrap.getIntHeader("b")==2)
+  }
+
+  test("Ensure getDateHeader can parse a supported date format") {
+    val req = request("GET","/foo","application/XML","",false,Map("Last-Modified"->List("Wed, 01 Jan 1970 00:00:01 GMT")))
+    val wrap = new CheckerServletRequest(req)
+
+    assert(wrap.getDateHeader("Last-Modified")==1000)
+  }
+
+  test("Ensure getDateHeader throws an IllegalArgumentException when an unsupported date format is provided") {
+    val req = request("GET","/foo","application/XML","",false,Map("Last-Modified"->List("01/01/1970/00/00/00 GMT")))
+    val wrap = new CheckerServletRequest(req)
+
+    intercept[IllegalArgumentException] {
+      wrap.getDateHeader("Last-Modified")
+    }
+  }
 }
