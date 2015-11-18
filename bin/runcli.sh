cdc61c00ad70297b067da392ecdd5e62976cc5c1
Jorge Williams <jorge.williams@rackspace.com>
2015-11-17 23:07:48 -0600

    Minor cleanups on runcli.sh
    We now check the JAVA environment variable, we do a better job of
    forming the path the CLI jar file, and we cleaned up some of the error
    messages.

--- bin/runcli.sh
+++ bin/runcli.sh
@@ -2,11 +2,16 @@
 ##  Runs the cli script $PROG
 ##
 
-if [ ! $PROG ] ; then { echo 'Missing $PROG environment variable. You should not call runcli.sh directly' ; exit 1; } fi
+if [ ! $PROG ] ; then
+    echo 'Missing $PROG environment variable. You should not call runcli.sh directly.'
+    exit 1
+fi
 
-JAVA=$(which java)
+if [ ! $JAVA ] ; then
+    JAVA=$(which java)
+fi
 
-command -v $JAVA >/dev/null 2>&1 || { echo $PROG ' requires java in the $PATH!'; exit 1; }
+command -v $JAVA >/dev/null 2>&1 || { echo $PROG ' requires $JAVA enviornment variable or java in the system path!'; exit 1; }
 
 VERSION=$($JAVA -version 2>&1 | grep ' version' | awk '{ print substr($3, 2, length($3)-2); }')
 JAVA_MINOR=$(echo $VERSION | tr "." " " | cut -d " " -f2)
@@ -17,14 +22,14 @@ if [ "$JAVA_MINOR" -lt "7" ]; then
   exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-WTESTJAR=$DIR/../cli/$PROG/target/$PROG*-with-*.jar
+WTESTJAR=$DIR/cli/$PROG/target/$PROG*-with-*.jar
 
 if [ -e $WTESTJAR ]; then
     $JAVA $JAVA_OPTS -jar $WTESTJAR "$@"
 else
     echo "Couldn't find $WTESTJAR "
-    echo -n "perhaps the project has not been built, "
+    echo -n "Perhaps the project has not been built, "
     echo "try building the project with mvn install."
 fi
